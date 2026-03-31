ac41f50a8129acdcf4a3975f7747f12c803ff4a0
Pedro Motta <163004710+xjott@users.noreply.github.com>
2026-03-31 11:53:29 -0300

    continuando o cadastro

---
+++ app/components/HeaderSignOut.tsx
@@ -0,0 +1,11 @@
+export default function HeaderSignOut(){
+    return(
+        <>
+        {/* Seção branca */}
+            <div className='w-full bg-white h-20 flex flex-row align-items justify-center'>
+                <div className='font-black'>Sorri</div>
+                <div className='text-p'>Sync</div>
+            </div>
+    </>
+    )
+}
