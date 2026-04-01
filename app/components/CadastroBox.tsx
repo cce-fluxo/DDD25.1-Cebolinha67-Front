d3bb66a78bdd648c27c6a442133f8109f156da01
Pedro Motta <163004710+xjott@users.noreply.github.com>
2026-04-01 06:45:19 -0300

    commit

--- app/components/CadastroBox.tsx
+++ app/components/CadastroBox.tsx
@@ -1,7 +1,7 @@
 export default function CadastroBox({children}: {children: React.ReactNode}){
     return(
-        <div className="mb-40">
-            <div className="bg-white rounded-3xl p-8 w-110 h-141 text-black text-center font-bold">
+        <div className="mt-20 mb-10">
+        <div className="flex w-145.75 h-176.75 px-19.25 py-6 flex-col align-middle gap-2.5 bg-white">
                 <p className="mt-7">Crie sua nova conta</p>
                 {children}
             </div>
