c5a6f58851b8e7e4624f7eb4146392d9dd0b5722
Pedro Motta <163004710+xjott@users.noreply.github.com>
2026-04-01 06:50:19 -0300

    commitando

--- app/components/HeaderSignOut.tsx
+++ app/components/HeaderSignOut.tsx
@@ -1,10 +1,16 @@
-export default function HeaderSignOut(){
-    return(
-        <>
-        {/* Seção branca */}
-            <div className='w-full bg-white h-26 flex flex-row items-center justify-center'>
-                <img src="/SorriSyncLogo.png" alt="SorriSync" className="h-10"></img>
-            </div>
-    </>
-    )
+import Image from "next/image";
+
+export default function HeaderSignOut() {
+  return (
+    <div className="flex justify-center h-[12.5%] pt-6 pb-6 bg-white">
+      {/*Logo SorrySync */}
+      <Image
+        src="/SorriSyncLogo.png"
+        alt="Logo"
+        width={300}
+        height={150}
+        className="h-full w-auto"  // melhorar essa header do arthur (aka botar mais coisas no tailwind)
+      />
+    </div>
+  );
 }
