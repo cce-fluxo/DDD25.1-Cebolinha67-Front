2fa21d881c527b44bb6b89cf8ab3d511bba8fc0e
tucoramos <tuturamos17@gmail.com>
2026-04-02 10:32:31 -0300

    Esqueleto da página home

--- app/sw/home/page.tsx
+++ app/sw/home/page.tsx
@@ -1,3 +1,7 @@
 export default function HomePage() {
-  return <main></main>;
+  return (
+    <main className="mx-auto grid w-full max-w-378 grid-cols-1 gap-4 px-3 py-4 lg:grid-cols-[355px_minmax(0,1fr)] lg:items-start">
+      <aside className="space-y-4"></aside>
+    </main>
+  );
 }
