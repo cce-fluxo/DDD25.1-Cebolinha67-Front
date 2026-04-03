f203cc43944e69d5516f343c1873418ad69c9898
Danilo Santiago <danilo.santiago.20241@poli.ufrj.br>
2026-04-03 16:36:47 -0300

    Page Esqueci minha senha criada

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import HomemNoPc from "./components/HomemNoPc";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen w-screen">
+    <div className="flex flex-col h-screen w-screen"> {/*verificar h-screen*/}
       {/*Barra de cabeçalho branca com a logo */}
       <HeaderLogin />
       {/* Parte principal da página */}
