14d6353043d218207b17a2980c721eded4e83f66
Danilo Santiago <danilo.santiago.20241@poli.ufrj.br>
2026-04-05 16:02:42 -0300

    Adição de href em EsqueciSenha.tsx da página de Login

--- app/components/EsqueciSenha.tsx
+++ app/components/EsqueciSenha.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 export default function EsqueciSenha() {
   return (
     <div className="text-right">
+      {/* Quando fizermos o deploy esse href mudará */}
       <Link
-        href=""
+        href="http://localhost:3000/esqueciSenha" 
         className="text-[#5754DE] text-[15px] font-lato font-extrabold"
       >
         Esqueci minha senha
