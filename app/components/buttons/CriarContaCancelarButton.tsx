9adca8066932f57f664bd9f3f568f83d34e25354
Pedro Motta <163004710+xjott@users.noreply.github.com>
2026-04-10 11:15:36 -0300

    agora sim

--- app/components/buttons/CriarContaCancelarButton.tsx
+++ app/components/buttons/CriarContaCancelarButton.tsx
@@ -11,13 +11,14 @@ export default function CriarContaCancelarButton({ habilitado }: CriarContaCance
   return (
     <div className="flex flex-col gap-3 mt-8 md:mt-8 justify-center items-center w-full">
       <button 
-        className={`py-2 rounded-xl w-40 text-white transition-opacity ${habilitado ? "bg-indigo-500" : "bg-indigo-300 cursor-not-allowed"}`}
+      onClick={() => router.push("/sw/home")}
+        className={` cursor-pointer py-2 rounded-xl w-40 text-white transition-opacity ${habilitado ? "bg-indigo-500" : "bg-indigo-300 cursor-not-allowed"}`}
         disabled={!habilitado}
       >
         Criar Conta
       </button>
       <button 
-        className="bg-indigo-200 text-black py-2 rounded-xl w-40"
+        className="bg-indigo-200 text-black py-2 rounded-xl w-40 cursor-pointer"
         onClick={() => router.back()}
       >
         Cancelar
--- app/components/buttons/VoltarContinuarButton.tsx
+++ app/components/buttons/VoltarContinuarButton.tsx
@@ -11,13 +11,13 @@ export default function VoltarContinuarButton({ habilitado }: VoltarContinuarBut
   return (
     <div className="flex gap-3 mt-8 md:mt-12 justify-center w-full">
       <button 
-        className="bg-indigo-200 text-black px-6 py-2 rounded-xl font-medium" 
+        className="bg-indigo-200 text-black px-6 py-2 rounded-xl font-medium cursor-pointer" 
         onClick={() => router.back()}
       >
         Voltar
       </button>
       <button 
-        className={`px-10 py-2 rounded-xl font-medium text-white transition-opacity ${habilitado ? "bg-indigo-500" : "bg-indigo-300 cursor-not-allowed"}`}
+        className={` cursor-pointer px-10 py-2 rounded-xl font-medium text-white transition-opacity ${habilitado ? "bg-indigo-500" : "bg-indigo-300 cursor-not-allowed"}`}
         onClick={() => habilitado && router.push("/auth/cadastro/crie-sua-senha")}
         disabled={!habilitado}
       >
