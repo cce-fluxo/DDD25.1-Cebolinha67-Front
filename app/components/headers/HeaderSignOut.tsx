44e581d6822357dd77bf0771608420e5bf2e2330
Danilo Santiago <danilo.santiago.20241@poli.ufrj.br>
2026-04-15 16:22:32 -0300

    deleção de componentes do arthur

--- app/components/headers/HeaderSignOut.tsx
+++
@@ -1,7 +0,0 @@
-export default function HeaderSignOut() {
-  return (
-    <div className="flex w-full h-16 md:h-24 px-6 flex-col justify-center items-center gap-2.5 bg-white relative">
-      <img src="/SorriSyncLogo.png" alt="sorrisync" className="w-24 md:w-28 h-auto" />
-    </div>
-  )
-}
