5230bd962941273ca9b00b87bdd08bd28021a277
Pedro Motta <163004710+xjott@users.noreply.github.com>
2026-04-16 13:43:41 -0300

    implmentando o popup

--- app/auth/cadastro/crie-sua-senha/page.tsx
+++ app/auth/cadastro/crie-sua-senha/page.tsx
@@ -11,8 +11,10 @@ import { useFormik } from "formik";
 import { schemaSenha } from "./crieSuaSenhaSchema";
 import { useAuth } from "@/app/context/AuthContext";
 import { useState } from "react";
+import PopUp from "@/app/components/popups/popupContaCriada";
 
 export default function CrieSuaSenha(){
+    const [popUpIsOpen, setpopUpIsOpen] = useState(false)
     const router = useRouter()
     const { login } = useAuth()
     const [erroCadastro, setErroCadastro] = useState<string | null>(null)
@@ -40,12 +42,17 @@ export default function CrieSuaSenha(){
           await login(dadosSalvos.email_usuario, values.senha)
 
           sessionStorage.removeItem("cadastro_dados")
-          router.push("/sw/home")
-        } catch (error: any) {
+          setpopUpIsOpen(true)
+          setTimeout(()=> {
+            router.push("/sw/home")
+          },3000)
+                } catch (error: any) {
           setErroCadastro(error.message)
         }
       }
     })
+
+// coloquei o set timeout pro popup só aparecer por um tempo específico na tela 
 
     return(
         <div>
@@ -70,6 +77,7 @@ export default function CrieSuaSenha(){
 
                     <CriarContaCancelarButton habilitado={formik.isValid && formik.dirty} onCriarConta={() => formik.submitForm()}></CriarContaCancelarButton>
                 </CrieSuaSenhaBox>
+              <PopUp título="Conta criada com sucesso!" BotaoTexto="" onPressBotao={() => null} isVisible={popUpIsOpen} toggleModal={()=> "sim"} children></PopUp> {/* continuar o popup */ }
             </BackgroundSignOut>
         </div>
     )
---
+++ app/components/popups/popupContaCriada.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import React from 'react';
+import BotaoEntrar from '../componentes-do-arthur/BotaoEntrar';
+
+interface PopUpProps {
+    título: string;
+    BotaoTexto: string;
+    onPressBotao: () => void;
+    isVisible: boolean;
+    toggleModal: () => void;
+    children?: React.ReactNode;
+}
+
+export default function PopUp({
+    título,
+    BotaoTexto,
+    onPressBotao,
+    isVisible,
+    toggleModal,
+}: PopUpProps) {
+
+    if (!isVisible) return null;
+
+    return (
+        <div 
+            className="fixed inset-0 flex items-center justify-center bg-black/50 p-7"
+            onClick={toggleModal}
+        >
+            <div className='bg-white text-[#5754DE] gap-4 flex flex-col items-center justify-center rounded-lg p-7'>
+                <div className='text-2xl'>
+                 {título}
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
